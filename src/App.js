04e1d6ac64f14b1513205d648bf951fa86523d83
Zeeshan Safdar <zeeshan7826@gmail.com>
2022-06-30 22:51:58 +0500

    files & folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import './App.css';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
