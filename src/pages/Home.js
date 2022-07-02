8f136749e0d77f5f0893857a07a63f596657c44c
Zeeshan Safdar <zeeshan7826@gmail.com>
2022-07-02 16:28:34 +0500

    exercise component search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
     <Box>
       <HeroBanner />
       <SearchExercises bodyPart={bodyPart} setBodyPart={setBodyPart} setExercises={setExercises} />
-      <Exercises bodyPart={bodyPart} setBodyPart={setBodyPart} setExercises={setExercises} />
+      <Exercises bodyPart={bodyPart} exercises={exercises} setExercises={setExercises} />
     </Box>
   );
 }
