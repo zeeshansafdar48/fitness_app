745e296ce0eb107b9e29310d25dcd6b7f68f098b
Zeeshan Safdar <zeeshan7826@gmail.com>
2022-07-02 12:18:11 +0500

    worked on hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 }
 
 export default Home;
